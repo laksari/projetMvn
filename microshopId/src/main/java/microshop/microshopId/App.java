d7e26700d6c36e838b45431ed2d655c95275270b
ajc <ajc@admin-PC>
2018-02-21 17:38:44 +0100

    commit2 kt

--- microshopId/src/main/java/microshop/microshopId/App.java
+++ microshopId/src/main/java/microshop/microshopId/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World kt !" );
+        System.out.println( "Hello World kt kt2!" );
     }
 }
